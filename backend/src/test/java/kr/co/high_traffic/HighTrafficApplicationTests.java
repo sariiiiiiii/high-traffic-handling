43ea72a14e45669d8133c5d33f9ab185574e239c
sariiiiiiii <abdf13423@naver.com>
2024-10-08 16:07:17 +0900

    first commit

---
+++ backend/src/test/java/kr/co/high_traffic/HighTrafficApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.high_traffic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighTrafficApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
